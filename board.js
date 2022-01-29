459202add6a7e9a1e20c261d6c5c4e0bb6b1183b
prerak-123 <preerakcontractor@gmail.com>
2022-01-29 19:11:14 +0530

    Init

---
+++ board.js
@@ -0,0 +1,23 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+//Game Dimensions
+var squareDimension = 40;
+
+// Object Square for each square of game.
+//Color Status 0 = Grey
+
+let Square = {
+    coordinateX:0,
+    coordinateY:0,
+    colorStatus:0
+}
+
+var squares = [];
+
+for (let index = 0; index < 200 ; index++) {
+    var square = new Square;
+    square.coordinateX = (index%10+10)*squareDimension
+    square.coordinateY = (Math.ceil(index/10))*squareDimension;
+    squares[i] = square;
+}
