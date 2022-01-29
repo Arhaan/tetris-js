ec71fd2a842a0bda733ec9c7c4055823f3a1ab9e
prerak-123 <preerakcontractor@gmail.com>
2022-01-29 19:22:09 +0530

    Init

--- tetris.js
+++ tetris.js
@@ -1,35 +1,24 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    var squareSide = 40;
 
-    //Main Screen of Tetris
-    ctx.beginPath();
-    for(let i = 1; i <= 20; i++){
-        for(let j = 10; j < 20; j++){
-            var x_coordinate = j*squareSide;
-            var y_coordinate = i*squareSide;
-            ctx.rect(x_coordinate, y_coordinate, squareSide, squareSide);
-        }
-    }
-    ctx.fillStyle = " #cfcfc7"
-    ctx.fill();
-    ctx.stroke();
-    ctx.closePath();
+//Game Dimensions
+var squareDimension = 40;
 
-    //Display for next block
+// Object Square for each square of game.
+//Color Status 0 = Grey
 
-    ctx.beginPath();
-    ctx.rect(1000, 40, 400, 55);
-    ctx.rect(1000, 95, 400, 400);
-    ctx.stroke();
-    ctx.closePath();
+let Square = {
+    coordinateX:0,
+    coordinateY:0,
+    colorStatus:0
+}
 
-    ctx.beginPath();
-    ctx.font = "30px Comic Sans MS";
-    ctx.fillStyle = "red";
-    ctx.textAlign = "center";
-    ctx.fillText("Next Block",1200 ,75);
-    ctx.closePath();
+var squares = [];
+
+for (let index = 0; index < 200 ; index++) {
+    var square = new Square;
+    square.coordinateX = (index%10+10)*squareDimension
+    square.coordinateY = (Math.ceil(index/10))*squareDimension;
+    squares[i] = square;
+}
     
